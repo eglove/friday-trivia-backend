7a51bc7217ac48a1799f89a29c0d1558192e467a
Ethan Glover <hello@ethang.email>
2021-03-27 13:41:55 -0500

    fixup

--- schemas/Option.ts
+++ schemas/Option.ts
@@ -1,15 +1,15 @@
 import { list } from '@keystone-next/keystone/schema';
-import {integer, relationship, text} from '@keystone-next/fields';
+import { integer, relationship, text } from '@keystone-next/fields';
 
 export const Option = list({
   fields: {
     content: text({ isRequired: true }),
     votes: integer(),
-    question: relationship({ref: 'Question.option', many: true})
+    question: relationship({ ref: 'Question.option', many: true }),
   },
   ui: {
     listView: {
-      initialColumns: ['votes', 'content']
-    }
-  }
+      initialColumns: ['votes', 'content'],
+    },
+  },
 });
