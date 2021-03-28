3dc2c7d483c873f7195d345eec627adf83d07bb7
Ethan Glover <hello@ethang.email>
2021-03-27 20:16:46 -0500

    option fixup

--- schemas/Option.ts
+++ schemas/Option.ts
@@ -4,7 +4,7 @@ import { integer, relationship, text } from '@keystone-next/fields';
 export const Option = list({
   fields: {
     content: text({ isRequired: true }),
-    votes: integer(),
+    votes: integer({ defaultValue: 1 }),
     question: relationship({ ref: 'Question.option', many: true }),
     usersVoted: relationship({ ref: 'User.votedOnOptions', many: true }),
   },
